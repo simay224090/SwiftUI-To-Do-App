92e93b73eb8f3147d15a7fde9f4c946284fea8ba
Simay Çalışkan <simaycaliskan@Simay-MacBook-Air.local>
2026-02-07 18:23:23 +0300

    First commit -SwiftUI Todo App

---
+++ MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Simay Çalışkan on 7.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
